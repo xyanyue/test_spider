e12a161b97d32da7f400721136656f2b14fed143
xyanyue <342807405@qq.com>
2018-04-02 17:07:53 +0800

    Update spider_func.py

--- spider_func.py
+++ spider_func.py
@@ -100,105 +100,18 @@ def result_user_first(self,url,level,soup,t_name,*,param={}):
 
 ###医院列表
 def result_user_second(self,url,level,soup,t_name,*,param={}):
-    print(level,t_name,url)
-    list_ = soup.findAll(name='ul',attrs={"class":"dis_n_list_area"})
-    prov = soup.find(name='div',attrs={"class":"head_tit"}).string.replace('地区医院','')
-
-    for x3 in list_:
-        hospital_type = x3.find_previous_sibling(name='div',attrs={"class":"hos_bj"}).string
-        for x4 in x3.findAll(name='li'):
-            u = x4.a['href']
-            insertQueue(self,"https:"+u,'three',t_name)
-            hs = x4.find_all('p')
-
-            hospital_name = hs[0].string
-            hospital_other = hs[1].string.split()
-            hospital_area = hospital_other[0] if (len(hospital_other) >= 1) else ''
-            hospital_level = hospital_other[1] if (len(hospital_other) >= 2) else ''
-
-            data = {
-                'area_p':prov,
-                'area_c':hospital_area,
-                'hospital_leixing':hospital_type,
-                'hospital_dengji':hospital_level
-            }
-            insertHospitalAndUpdate(self,data,hospital_name,t_name)
-    updateUrlListStatus(self,url,t_name)
+    pass
 
 ###医院详细信息 以及科室列表
 def result_user_three(self,url,level,soup,t_name,*,param={}):
-    print(level,t_name,url)
-    tel_replace = ['<span class="tel">','</span','<p class="hos-tel fl clearfix">','</p>',' ',"\n"]
-    hospital_name = soup.find(name='div',attrs={"class":"hos-name"}).p.string
-
-    doctor_num = soup.find(name='p',attrs={"class":'doctor-num'})
-    if doctor_num is not None:
-        doctor_num = doctor_num.i.string
-    depart_num = soup.find(name='p',attrs={"class":'Depart-num'})
-    if depart_num is not None:
-        depart_num = depart_num.i.string
-
-    hos_add = soup.find(name='p',attrs={"class":'hos-add'})
-    if hos_add is not None:
-        hos_add = hos_add.string
+    pass
     
-    hos_tel = soup.find(name='p',attrs={"class":'hos-tel'})
-    if hos_tel is not None:
-        hos_tel = str(hos_tel)
-        if hos_tel:
-            for rep in tel_replace:
-                hos_tel = hos_tel.replace(rep,'')
-
-    # print(hos_tel.strip())
-    data = {
-        'doc_num':doctor_num,
-        'class_num':depart_num,
-        'address':hos_add,
-        'phone':hos_tel
-    }
-    print(data)
-    insertHospitalAndUpdate(self,data,hospital_name,t_name)
-   
-
-    intro_url = soup.find(name='a',attrs={"id":'HospitalIntroduction_more'})
-    if intro_url is not None:
-        pass
-        # insertQueue(self,intro_url['href'],'four',t_name)
-
-
-    depart_list = soup.findAll(name='ul',attrs={"class":"depart-list"})
-    for x in depart_list:
-        first_level = x.find(name='li',attrs={"class":'first-level'}).string
-        depart_item = x.findAll(name='li',attrs={"class":'faculty_list_cnzz'})
-        for x2 in depart_item:
-            depart = x2.a.span.string
-            depart_url = x2.a['href']
-            # insertQueue(self,depart_url,'five',t_name)
-            depart_num = x2.a.i.string
-            d={
-                'parent_depart':first_level,
-                'depart':depart,
-                'num':depart_num,
-            }
-            w = {
-                'hospital_name':hospital_name,
-                'depart':depart
-            }
-            insertAllAndUpdate(self,d,w,t_name,'haodf_hospital_depart')
-            # print(d)
-    updateUrlListStatus(self,url,t_name)
 
 
 
 
 def result_user_four(self,url,level,soup,t_name,*,param={}):
-    print(level,t_name,url)
-    hospital_name = soup.find(name='p',attrs={"class":"hos_intro"}).string.replace('简介','')
-    intro = soup.find(name='p',attrs={'class':'gray6'}).get_text()
-    # print(str(intro))
-    intro = intro.replace('%','%%')
-    insertHospitalAndUpdate(self,{'intro':intro},hospital_name,t_name)
-    updateUrlListStatus(self,url,t_name)
+    pass
 
 # def before_user_five(self,url,level,t_name,*,param={}):
 #     ##https://m.haodf.com/touch/faculty/loaddoctors/DE4roiYGYZwWGX0voi5u5tiO7?p=19
@@ -220,42 +133,7 @@ def result_user_four(self,url,level,soup,t_name,*,param={}):
     # return True,'json',url,{'hospital_name':hospital_name}
 
 def result_user_five(self,url,level,soup,t_name,*,param={}):
-    print(level,t_name,url)
-    hospital_name = soup.find(name='a',attrs={'id':'cnzz_mianbao_hospital'}).string
-    depart = soup.find(name='a',attrs={'id':'cnzz_mianbao_faculty'}).string
-    # depart = hospital_depart[1]
-    his_url = url
-    u = url.replace('https://m.haodf.com/touch/faculty/','').replace('.htm','')
-    url = "https://m.haodf.com/touch/faculty/loaddoctors/"+u
-    page = 0
-    flag = True
-    where = {'hospital_name':hospital_name,'depart':depart}
-    while flag:
-        url_now =url+"?p="+str(page)
-        Html = self.runUrl(url_now)
-        
-        if Html :
-            ret = json.loads(Html)
-            # print(ret)
-            if ret['finished'] :
-                flag = False
-                # continue
-            for v in ret['contents']:
-                
-                datas = {
-                    'level':v['grade'],
-                    'edu_level':v['educateGrade'],
-                    'headimg':v['smallHeadImage'],
-                    'specializeList':v['specializeList'],
-                    'schedule':v['schedule']
-                }
-                where['doc_name'] = v['name']
-                insertAllAndUpdate(self,datas,where,t_name,'haodf_depat_docter')
-            print(page)
-            page = page +1
-    updateUrlListStatus(self,his_url,t_name)
-
-
+    pass
 
 
 

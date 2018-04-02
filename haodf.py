11e84d8ff67281f73bd4edc4eece5844169f4b5c
xyanyue <342807405@qq.com>
2018-04-02 16:45:50 +0800

    Update haodf.py

--- haodf.py
+++ haodf.py
@@ -2,12 +2,15 @@
 # -*- coding:utf-8 -*-
 
 
+#######请不要直接运行谢谢##########
+#######本实例是测试所写，由此产生的法律问题请自行承担#############
+
 
 import sys
 import spider_class
 
 LevelName = ['first','second','three','four','five']
-firstUrl = "https://m.haodf.com/touch/province/list.htm"
+firstUrl = "https://m.haodf.com/touch/province/listxxxxxxxxxxx.htm"
 
 s = spider_class.Spider(firstUrl=firstUrl, LevelName=LevelName)
 # s = spider_class.Spider(back=True, LevelName=LevelName)
